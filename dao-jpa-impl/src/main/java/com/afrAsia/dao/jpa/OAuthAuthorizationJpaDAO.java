dbe38d42d941974c0d94f6e2108328cd9b615520
Nyal Fernandes <midnightoil.nyal@gmail.com>
2017-07-07 01:59:42 +0530

    Added oAuthAuthorisationDAO and userDAO

--- dao-jpa-impl/src/main/java/com/afrAsia/dao/jpa/OAuthAuthorizationJpaDAO.java
+++ dao-jpa-impl/src/main/java/com/afrAsia/dao/jpa/OAuthAuthorizationJpaDAO.java
@@ -1,8 +1,9 @@
 package com.afrAsia.dao.jpa;
 
+import com.afrAsia.dao.OAuthAuthorizationDAO;
 import com.afrAsia.entities.jpa.OauthAuthorization;
 
-public interface OAuthAuthorizationJpaDAO extends BaseJpaDAO<String, OauthAuthorization>
+public interface OAuthAuthorizationJpaDAO extends BaseJpaDAO<String, OauthAuthorization>, OAuthAuthorizationDAO
 {
     public OauthAuthorization getOauthAuthorizationByClientId(String clientId);
 }
--- dao-jpa-impl/src/main/java/com/afrAsia/dao/jpa/impl/OAuthAuthorizationJpaDAOImpl.java
+++ dao-jpa-impl/src/main/java/com/afrAsia/dao/jpa/impl/OAuthAuthorizationJpaDAOImpl.java
@@ -6,19 +6,50 @@ import java.util.Map;
 import com.afrAsia.dao.jpa.OAuthAuthorizationJpaDAO;
 import com.afrAsia.entities.jpa.OauthAuthorization;
 
-public class OAuthAuthorizationJpaDAOImpl extends
-		BaseJpaDAOImpl<String, OauthAuthorization> implements
-		OAuthAuthorizationJpaDAO {
+public class OAuthAuthorizationJpaDAOImpl extends BaseJpaDAOImpl<String, OauthAuthorization>
+		implements OAuthAuthorizationJpaDAO 
+{
 
-	public OauthAuthorization getOauthAuthorizationByClientId(String clientId) {
+	public OauthAuthorization getOauthAuthorizationByClientId(String clientId) 
+	{
 		Map<String, Object> parameters = new HashMap<String, Object>();
-		//Client client = clientJpaDAO.findById(clientId);
-//		if(oauthAuthorization!=null){
-//			oauthAuthorization.setClient(client);
-//		}
+		// Client client = clientJpaDAO.findById(clientId);
+		// if(oauthAuthorization!=null){
+		// oauthAuthorization.setClient(client);
+		// }
 		parameters.put("clientId", clientId);
-		return findByNamedQuery(
-				OauthAuthorization.QUERY_FIND_BY_CLIENT_ID, parameters);
+		return findByNamedQuery(OauthAuthorization.QUERY_FIND_BY_CLIENT_ID, parameters);
+	}
+
+	public void saveClientDetail(OauthAuthorization oauthAuthorization) 
+	{
+		if (oauthAuthorization != null)
+		{
+			saveOrUpdate(oauthAuthorization);
+		}
+
+	}
+
+	public OauthAuthorization loadClientByClientId(String clientId) throws Exception 
+	{
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		// Client client = clientJpaDAO.findById(clientId);
+		// if(oauthAuthorization!=null){
+		// oauthAuthorization.setClient(client);
+		// }
+		parameters.put("clientId", clientId);
+		return findByNamedQuery(OauthAuthorization.QUERY_FIND_BY_CLIENT_ID, parameters);
+	}
+
+	public OauthAuthorization getClient(String clientId) 
+	{
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		// Client client = clientJpaDAO.findById(clientId);
+		// if(oauthAuthorization!=null){
+		// oauthAuthorization.setClient(client);
+		// }
+		parameters.put("clientId", clientId);
+		return findByNamedQuery(OauthAuthorization.QUERY_FIND_BY_CLIENT_ID, parameters);
 	}
 
 }
